17303f2c1798a2ef32169c594a2eccd0dbc9b408
Boris Khalkov <boris.khalkov@gmail.com>
2021-12-27 14:06:54 +0300

    carousel markup done

---
+++ Pulse/src/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.carousel__inner').slick({
+		autoplay: true,
+		adaptive:false,
+		prevArrow:`<button type="button" class="slick-prev"><img src="img/slider/chevron-left-solid.png" alt="right"></button>`,
+		nextArrow:`<button type="button" class="slick-next"><img src="img/slider/chevron right solid.png" alt="right"></button>`,
+	});
+  });
