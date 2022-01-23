18a6a779e8a29eddb01e219f2931a24e529c9d65
Boris Khalkov <boris.khalkov@gmail.com>
2022-01-23 13:28:06 +0300

    section catalog added

--- Pulse/gulpfile.js
+++ Pulse/gulpfile.js
@@ -4,30 +4,36 @@ const sass         = require('gulp-sass')(require('sass'));
 const cleanCSS     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename       = require("gulp-rename");
+const { series } = require('gulp');
+sass.compiler      = require('sass');
 
 gulp.task('server', function() {
 
-    browserSync({
-        server: {
-            baseDir: "src"
-        }
-    });
+	browserSync({
+		server: {
+			baseDir: "src"
+		}
+	});
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+	exports.src=series('watch', 'html', 'server', 'styles');
 });
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+	gulp.task('html', function() {
+		gulp.watch("src/*.html").on('change', browserSync.reload);
+	});
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+	gulp.task('styles', function() {
+		return gulp.src("src/sass/**/*.+(scss|sass)")
+		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+		.pipe(rename({suffix: '.min', prefix: ''}))
+		.pipe(autoprefixer())
+		.pipe(cleanCSS({compatibility: 'ie8'}))
+		.pipe(gulp.dest("src/css"))
+		.pipe(browserSync.stream());
+	});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+	gulp.task('watch', function() {
+		gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+	})
+
+gulp.task('default', gulp.parallel('watch', 'html', 'server', 'styles'));
--- Pulse/src/js/main.js
+++ Pulse/src/js/main.js
@@ -1,8 +1,28 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	$('.carousel__inner').slick({
 		autoplay: true,
-		adaptive:false,
-		prevArrow:`<button type="button" class="slick-prev"><img src="img/slider/chevron-left-solid.png" alt="right"></button>`,
-		nextArrow:`<button type="button" class="slick-next"><img src="img/slider/chevron right solid.png" alt="right"></button>`,
+		adaptive: false,
+		prevArrow: `<button type="button" class="slick-prev"><img src="img/slider/chevron-left-solid.png" alt="right"></button>`,
+		nextArrow: `<button type="button" class="slick-next"><img src="img/slider/chevron right solid.png" alt="right"></button>`,
 	});
-  });
+
+	$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function () {
+		$(this)
+			.addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+			.closest('div.container').find('div.catalog__content').removeClass('catalog__content-active').eq($(this).index()).addClass('catalog__content-active');
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
+			})
+		});
+	};
+
+	toggleSlide('.catalog-item__link');
+	toggleSlide('.catalog-item__back');
+
+});
