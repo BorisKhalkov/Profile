11150cef9b1192754b4ace2733f70e75de2534a9
Boris Khalkov <boris.khalkov@gmail.com>
2022-03-13 17:52:22 +0300

    Fixed catalog details

--- Pulse/dist/js/main.js
+++ Pulse/dist/js/main.js
@@ -109,7 +109,6 @@ $(document).ready(function () {
 
 	$("a[href^='#']").click(function () {
 		let _href = $(this).attr("href");
-		console.log(_href);
 		$('html, body').animate({scrollTop: $(_href).offset().top+'px'});
 		return false;
 	});
